5cb6661c7fdabd806bccdecce90bb0c3b83825cb
srstafford1996 <srstafford1996@gmail.com>
2025-03-01 06:42:07 -0800

    Aprox 60 FPS render loop

--- src/main.c
+++ src/main.c
@@ -10,50 +10,75 @@
 #include "render.h"
 #include "input.h"
 
+const int FPS = 60;
 
-int main() {
+int main()
+{ 
+    bool isRunning = true;
     
+    // Frame timing
+    const Uint64 FRAME_DELAY = 1000 / FPS;
+    Uint64 frameStart = 0;
+    Uint64 frameTime = 0;    
+    Uint64 prevFrame = 0;
+    
+    // SDL Window Management
+    SDL_Event ev;    
     SDL_Window *window = NULL;
     SDL_Renderer *renderer = NULL;
-
-    if( SDL_Init(SDL_INIT_VIDEO) < 0 ) {
+    
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
         printf("Error initializing SDL: %s\n", SDL_GetError());
         SDL_Delay(2000);
         return 1;
     }
-
-    /*
-    if( !IMG_Init(0) ) {
-        printf("Error initializing SDL Image: %s\n", IMG_GetError());
-        SDL_Delay(2000);
-        return 1;
-    }
-        */
+    
+    IMG_Init(0);
+    
     window = SDL_CreateWindow("KBD Trainer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, INITIAL_VIEW_WIDTH, INITIAL_VIEW_HEIGHT, SDL_WINDOW_SHOWN);
-    if( window == NULL ) {
+    if (window == NULL)
+    {
         printf("Error creating window: %s\n", SDL_GetError());
         SDL_Delay(2000);
         return 1;
     }
-
+    
     renderer = SDL_CreateRenderer( window, -1, SDL_RENDERER_ACCELERATED );
-    if( renderer == NULL ) {
+    if (renderer == NULL)
+    {
         printf("Error creating renderer: %s\n", SDL_GetError());
         SDL_Delay(2000);
         return 1;
     }
     
-    if (Init_Textures(renderer)) {
+    if (Init_Textures(renderer))
         printf("Texture load success!\n");
-    }
-
-    SDL_RenderClear(renderer);
-    Render_KBD_Sequence(renderer);
-    SDL_RenderPresent(renderer);
-
     
-    SDL_Delay(2000);
+    
+    while (isRunning)
+    {
+        prevFrame = frameStart;
+        frameStart = SDL_GetTicks64();
+        
+        while (SDL_PollEvent(&ev) != 0)
+        {
+            if (ev.type == SDL_QUIT)
+                isRunning = false;
+        }
 
+        SDL_RenderClear(renderer);
+        Render_KBD_Sequence(renderer);
+        SDL_RenderPresent(renderer);
+
+        // How long it took to execute the functions
+        frameTime = SDL_GetTicks64() - frameStart;
+
+        //Wait out the remainder
+        if (FRAME_DELAY > frameTime) 
+            SDL_Delay(FRAME_DELAY - frameTime);
+    }
+    
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
